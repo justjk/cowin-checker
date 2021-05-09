7b3c7895ba204383ec7ccd3c47659d0a978fef00
Jayakrishnan GK <jayakrishnan.gk@elucidata.io>
2021-05-09 18:21:25 +0530

    added relevant env vars in cron job script

--- cronjob_entrypoint.sh
+++ cronjob_entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # set district ids of interest.
-DISTRICT_IDS=146,147
+DISTRICT_IDS=307,301,303,304
 
 #replace with relevant age
-AGE=33
+AGE=45
 
 
 #needed for notifications to work
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+#uncomment below and set value. needed for telegram notifications to work
+# export COWIN_CHECKER_TELEGRAM_CHANNEL_ID=<channel id>
+# export COWIN_CHECKER_TELEGRAM_BOT_TOKEN=<bot token>
 
 #replace with actual path of source code
 cd ~/personal/cowinchecker
